ba9ee10ea668e8838bc5ff67f6aa0089058e50f0
B-r-a-s-s <48525938+B-r-a-s-s@users.noreply.github.com>
2019-11-20 11:59:20 +0100

    ds

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ alpha = 75 # deg
 beta = 75 # deg
 T = 0.47 # torque per motor
 
+nm = 2 # number of motors
+
 # Worst case, fully flat
 alpha = 28 # deg
 beta = 15 # degrees
@@ -16,5 +18,5 @@ beta = math.radians(beta)
 
 Fy = T * np.sin(beta) / (a * np.cos(0.5*np.pi - alpha - beta))
 
-print('Vertical power, assuming two motors:',Fy*2,'N')
-print('Vertical power, assuming two motors:',Fy*2/9.81,'kg')
+print('Vertical power, assuming two motors:',Fy*nm,'N')
+print('Vertical power, assuming two motors:',Fy*nm/9.81,'kg')
