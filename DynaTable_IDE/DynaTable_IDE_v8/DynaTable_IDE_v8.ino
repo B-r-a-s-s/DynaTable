7a9e44cfcfaefa03da16c872ce2ecffffc606112
CKos1999 <45196623+CKos1999@users.noreply.github.com>
2019-12-19 09:16:24 +0100

    Update DynaTable_IDE_v8.ino

--- DynaTable_IDE/DynaTable_IDE_v8/DynaTable_IDE_v8.ino
+++ DynaTable_IDE/DynaTable_IDE_v8/DynaTable_IDE_v8.ino
@@ -69,8 +69,8 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Title:      DynaTable Arduino Program");
   Serial.println("Board:      Arduino Mega/Arduino Mega 2560");
-  Serial.println("Version:    7");
-  Serial.println("Date:       18/12/2019");
+  Serial.println("Version:    8");
+  Serial.println("Date:       19/12/2019");
   Serial.println("Author(s):  C. Kos, B. Cheizoo, J. de Wolde, T. Hartsuijker");
   Serial.println();
   
