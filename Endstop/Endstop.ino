3ea0d6bd6872478a6b948473bde82f2873f637fd
B-r-a-s-s <48525938+B-r-a-s-s@users.noreply.github.com>
2020-01-06 21:35:19 +0100

    Endstop code works now! :)

--- Endstop/Endstop.ino
+++ Endstop/Endstop.ino
@@ -3,20 +3,56 @@ const int ESP = 30;
 int ESI_Prev;
 int ESI;
 
+int edgeFlag = false;
+
+unsigned long edgeWait = 1000;
+unsigned long edgeTime;
+
+int checkFlag = false;
+
+// TEMPORARY TIMER (proof of concept)
+unsigned long checkLimit = 2000;
+unsigned long checkTimer = 0;
+
+/*
+ * ENDSTOP PIN WIRING
+ * 
+ * Low to ground
+ * Mid to nothing, maybe ground for completeness' sake
+ * High to input pin
+ */
+
 void setup() {
   pinMode(ESP, INPUT);
+  pinMode(ESP, INPUT_PULLUP);
+  
   ESI_Prev = digitalRead(ESP);
   
+  Serial.begin(115200);
+  Serial.println("Start-up");
+
 }
 
 void loop() {
+  
   ESI = digitalRead(ESP);
 
-  if (ESI != ESI_Prev) {
-    Serial.println(ESI);
-    ESI_Prev = ESI;
+  if (edgeFlag == true) {
+    if (millis() >= edgeTime + edgeWait) {
+      edgeFlag = false;
+      Serial.println("Flag set to false");
+    }
+  } else if (ESI == 0 && ESI_Prev == 1) {
+    edgeTime = millis();
+    edgeFlag = true;
+    checkFlag = false;
+    Serial.println("Falling edge detected");
   } else {
-    
   }
   
+  if (ESI != ESI_Prev) {
+    ESI_Prev = ESI;
+  } else {
+  }
+
 }
